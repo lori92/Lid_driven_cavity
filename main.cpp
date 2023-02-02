89ae41601bc1a6c5042adb3b74aad918dae8f298
Lorenzo Sufrà <67364072+lori92@users.noreply.github.com>
2023-02-02 01:02:52 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,17 +16,69 @@ int main()
  cell **mesh = buildMesh(Nx, Ny, Lx, Ly);
  cell **Ux   = buildMesh(Nx, Ny, Lx, Ly);
 
-// cell **Uy   = buildMesh_v(Nx, Ny, Lx, Ly);
+ cell **Uy   = buildMesh(Nx, Ny, Lx, Ly);
 // cell **p    = buildMesh(Nx, Ny, Lx, Ly);
 
- mesh[1][Nx+1].print_cellCentre();
+ //mesh[1][Nx+1].print_cellCentre();
 
- initMeshVal(Ux, Nx, Ny, 12.5);
- Ux  [1][Nx+1].print_cellVal();
+ initFieldVal(Ux, Nx, Ny, 12.5);
+ initFieldVal(Uy, Nx, Ny,  2.5);
+
+ setBCu(Ux, Nx, Ny);
+ setBCv(Uy, Nx, Ny);
+
+/*
+ for (int i = 0; i <= Nx+1; i++)
+ {
+   for(int j = 0; j<= Ny+1; j++)
+   {
+    Ux[i][j].print_cellVal();
+   }
+   cout<<"\n";
+ } 
+*/
+  for (int j = 1; j <= Ny; j++)
+ {
+   for(int i = 0; i<= Nx; i++)
+   {
+    Uy[i][j].print_cellVal();
+   }
+   cout<<"\n";
+ } 
+
+
+
+ //free(Uy);
+ //free(p);
+double fUU_f, fUU_b;
+double fUV_f, fUV_b;
+
+double conv_u;
+
+ for (int i = 1; i <= Nx; i++)
+ {
+   for(int j = 1; j<= Ny; j++)
+   {
+     fUU_f = 0.25 * (Ux[i][j].cellVal() + Ux[i][j].cellVal()) *(Ux[i+1][j].cellVal() + Ux[i+1][j].cellVal());
+     fUU_b = 0.25 * (Ux[i][j].cellVal() + Ux[i][j].cellVal()) *(Ux[i-1][j].cellVal() + Ux[i-1][j].cellVal());
+
+     fUV_f = 0.5*(Ux[i ][j+1].cellVal() + Ux[i][j].cellVal() ) * \
+                                  0.5* (  Uy[i][ j ].cellVal() + Uy[i+1][ j ].cellVal() );
+     fUV_b = 0.5*(Ux[i ][j ].cellVal() + Ux[i-1][j-1].cellVal() ) * \
+                                  0.5* (  Uy[i][ j ].cellVal() + Uy[i+1][j-1].cellVal() );
+
+     conv_u = (fUU_f - fUU_b) / (mesh[i][j].dx())  \
+             +(fUV_f - fUV_b) / (mesh[i][j].dy()); 
+
+   }
+   cout<<"\n";
+ } 
+
+
+
 
  free(mesh);
  free(Ux);
- //free(Uy);
- //free(p);
+ free(Uy);
  return 0;	
 }
--- mesh.cpp
+++ mesh.cpp
@@ -16,6 +16,12 @@ class cell
  void setVal (double);
  void print_cellCentre ();
  void print_cellVal ();
+ double cellVal ();
+ double dx ();
+ double dy ();
+
+
+
 };
 
 void cell :: setCellPos(double x, double y, double dx, double dy)
@@ -42,10 +48,21 @@ void cell :: print_cellCentre(){
         cout << "(" <<xC<<","<<yC<<") ";};
 
 void cell :: print_cellVal(){
-        cout << value;};
+        cout << value<<" ";};
+
+double cell :: cellVal(){
+       return this->value;};
+       
+double cell :: dx(){
+       return this->xFaces[1]-this->xFaces[0];};
+       
+double cell :: dy(){
+       return this->yFaces[1]-this->yFaces[0];};
 ////////////////////////////////////////////////////////////////
 cell** buildMesh (int N_x, int N_y, double L_x, double L_y)
 {
+ // index i spans the y-direction
+ // index j spans the x-direction
 
  cell **mesh_p = new cell* [N_x+2];
  for (int i = 0; i <= N_x+1; i++)
@@ -53,7 +70,7 @@ cell** buildMesh (int N_x, int N_y, double L_x, double L_y)
    mesh_p[i] = new cell [N_y+2];
    for(int j = 0; j<= N_y+1; j++)
    {       
-    mesh_p[i][j].setCellPos( L_x/N_x/2. +  (L_x/N_x)*(i-1), L_y/N_y/2. + (L_y/N_y)*(j-1), L_x/N_x, L_y/N_y  );
+    mesh_p[i][j].setCellPos( L_y/N_y/2. +  (L_y/N_y)*(i-1), L_x/N_x/2. + (L_x/N_x)*(j-1), L_y/N_y, L_x/N_x  );
    }     
  }       
 
@@ -61,7 +78,7 @@ cell** buildMesh (int N_x, int N_y, double L_x, double L_y)
 };
 
 ////////////////////////////////////////////////////////////////
-void initMeshVal (cell** mesh_p, int N_x, int N_y, double val_in)
+void initFieldVal (cell** mesh_p, int N_x, int N_y, double val_in)
 {
 
  for (int i = 0; i <= N_x+1; i++)
@@ -73,15 +90,46 @@ void initMeshVal (cell** mesh_p, int N_x, int N_y, double val_in)
  }
  return;
 };
-
-
-///////////////////////////////////////////////////////////////
-// generic class for finite volume field //
-class volumeField
+////////////////////////////////////////////////////////////////
+void setBCu (cell** mesh_p, int N_x, int N_y)
 {
- private:
-  cell** mesh;
-   
- public:
-  
+ // set wall boundary condition for staggered Ux velocity field  
+
+// along y-axis     
+ for (int j= 0; j <= N_y+1; j++)
+ {
+    mesh_p[ 0   ][j].setVal(0);
+    mesh_p[N_x  ][j].setVal(0);
+    mesh_p[N_x+1][j].setVal(0);
+ }
+
+// along x-axis     
+ for (int i = 0; i <= N_x+1; i++)
+ {
+    mesh_p[i][  0  ].setVal( -mesh_p[i][ 1 ].cellVal() )  ;
+    mesh_p[i][N_x+1].setVal( -mesh_p[i][N_x].cellVal() )  ;
+ }
+ return;
+};
+////////////////////////////////////////////////////////////////
+void setBCv (cell** mesh_p, int N_x, int N_y)
+{
+ // set wall boundary condition for staggered Uy velocity field  
+
+ // along y-axis        
+ for (int j = 0; j <= N_y+1; j++)
+ { 
+    mesh_p[  0  ][j].setVal( - mesh_p[ 1 ][j].cellVal()  );
+    mesh_p[N_x+1][j].setVal( - mesh_p[N_x][j].cellVal()  );
+ }
+
+ // along x-axis        
+ for (int i = 0; i <= N_x+1; i++)
+ {
+    mesh_p[i][ N_y ].setVal(0);
+    mesh_p[i][N_y+1].setVal(0);
+    mesh_p[i][  0  ].setVal(0);
+
+ }
+ return;
 };
