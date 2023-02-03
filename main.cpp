3e58981fdcbd19a69ab5628282dfa586cd0c9552
Lorenzo Sufrà <67364072+lori92@users.noreply.github.com>
2023-02-03 09:41:58 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,17 +9,21 @@ using namespace std;
 #define Nx 5
 #define Ny 5
 
+
+
 int main()
 {
 
  const double Lx = 1;
  const double Ly = 1;
 
- cell **mesh = buildMesh(Nx, Ny, Lx, Ly);
- cell **Ux   = buildMesh(Nx, Ny, Lx, Ly);
+ FVcell **mesh = buildMesh(Nx, Ny, Lx, Ly);
+ FVcell **Ux   = buildMesh(Nx, Ny, Lx, Ly);
+ FVcell **Uy   = buildMesh(Nx, Ny, Lx, Ly);
+ FVcell **Ux_star   = buildMesh(Nx, Ny, Lx, Ly);
+ FVcell **Uy_star   = buildMesh(Nx, Ny, Lx, Ly);
 
- cell **Uy   = buildMesh(Nx, Ny, Lx, Ly);
-// cell **p    = buildMesh(Nx, Ny, Lx, Ly);
+ volumeField p    = buildFVfield(Nx, Ny, Lx, Ly);
 
  //mesh[1][Nx+1].print_cellCentre();
 
@@ -71,7 +75,20 @@ double mu;
 double dt;
 double rho;
 
-list<cell**> Ux_time_series; 	
+dt = 0.001;
+double t0   = 0.;
+double tend = 0.001;
+double t;
+
+list<FVcell**> Ux_time_series; 	
+list<FVcell**> Uy_time_series; 	
+list<FVcell**> p_time_series; 	
+
+t = t0;
+
+while (t<=tend)
+{
+ t = t + dt;
 
  for (int i = 1; i <= Nx; i++)
  {
@@ -82,17 +99,17 @@ list<cell**> Ux_time_series;
      fUU_f = 0.25 * (Ux[i][j].cellVal() + Ux[i][j].cellVal()) *(Ux[i+1][j].cellVal() + Ux[i+1][j].cellVal());
      fUU_b = 0.25 * (Ux[i][j].cellVal() + Ux[i][j].cellVal()) *(Ux[i-1][j].cellVal() + Ux[i-1][j].cellVal());
 
-     fUV_f = 0.5*(Ux[i ][j+1].cellVal() + Ux[i][j].cellVal() ) * \
+     fUV_f = 0.5*rho*(Ux[i ][j+1].cellVal() + Ux[i][j].cellVal() ) * \
                                   0.5* (  Uy[i][ j ].cellVal() + Uy[i+1][ j ].cellVal() );
-     fUV_b = 0.5*(Ux[i ][j ].cellVal() + Ux[i-1][j-1].cellVal() ) * \
+     fUV_b = 0.5*rho*(Ux[i ][j ].cellVal() + Ux[i-1][j-1].cellVal() ) * \
                                   0.5* (  Uy[i][ j ].cellVal() + Uy[i+1][j-1].cellVal() );
 
      conv_u = (fUU_f - fUU_b) / (mesh[i][j].dx())  \
              +(fUV_f - fUV_b) / (mesh[i][j].dy()); 
 
      //// convective term d(vu)/dx +  d(vv)/dy -- v-control volume     
-     fVU_f = 0.5 * (Ux[i][j+1].cellVal() + Ux[i] [ j].cellVal()) * 0.5 * (Uy[i+1][ j ].cellVal() + Uy[i][ j ].cellVal() ); 
-     fVU_b = 0.5 * (Ux[i][ j ].cellVal() + Ux[i][j-1].cellVal()) * 0.5 * (Uy[i+1][j-1].cellVal() + Uy[i][j-1].cellVal() );       
+     fVU_f = 0.5 * rho*(Ux[i][j+1].cellVal() + Ux[i] [ j].cellVal()) * 0.5 * (Uy[i+1][ j ].cellVal() + Uy[i][ j ].cellVal() ); 
+     fVU_b = 0.5 * rho*(Ux[i][ j ].cellVal() + Ux[i][j-1].cellVal()) * 0.5 * (Uy[i+1][j-1].cellVal() + Uy[i][j-1].cellVal() );       
 
 
      conv_v = (fVU_f - fVU_b) / (mesh[i][j].dx())  \
@@ -122,14 +139,28 @@ list<cell**> Ux_time_series;
 
      diff_v = ddV_dxx + ddV_dyy;
 
-     
-
-
+     Ux_star[i][j].setVal( Ux[i][j].cellVal() - dt * (conv_u + diff_u) );
+     Uy_star[i][j].setVal( Uy[i][j].cellVal() - dt * (conv_v + diff_v) );
    }
    cout<<"\n";
  } 
+ setBCu(Ux_star, Nx, Ny);
+ setBCv(Uy_star, Nx, Ny);
 
+Ux_time_series.push_back(Ux_star);
 
+};
+
+/*
+ for (int i = Nx+1; i <= Nx+1; i++)
+ {
+   for(int j = 0; j<= Ny+1; j++)
+   {
+    Uy_star[i][j].print_cellVal();
+   }
+   cout<<"\n";
+ } 
+*/
 
 
  free(mesh);
@@ -137,3 +168,65 @@ list<cell**> Ux_time_series;
  free(Uy);
  return 0;	
 }
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+double l1_norm(FVcell** const field, int N_x, int N_y) 
+{
+    double accum = 0.;
+
+    for (int i = 1; i <= N_x; ++i) 
+    {
+         for (int j= 1; j <= N_y; ++i) 
+         {
+          accum += abs( field[i][j].cellVal() ) ;
+         };
+    };
+    return sqrt(accum);
+};
+/////////////////////////////////////////////////////////////////////////////////////////////
+void poisson (volumeField* p, FVcell** Ux_pred, FVcell** Uy_pred, double tol, double omega)
+{
+    double ratio = 1;
+    double l1norm, l1norm_old;
+
+    int N_x, N_y;
+    N_x = p->dimx;
+    N_y = p->dimy;
+
+    FVcell **p_ptr;
+
+    // initialize mesh pointer to input pointer to pressure
+    p_ptr = p->mesh; 
+
+    int niter = 0;
+
+    while (ratio > tol)
+    {
+        p_ptr = p->mesh; 
+        l1norm_old =  l1_norm (p_ptr, N_x, N_y);
+        niter+= 1;
+
+        for (int i = 1; i <= N_x; ++i) {
+            for (int j= 1; j <= N_y; ++i) {
+                //p_ptr[i][j].setVal( (1-omega)*(p[i][j].cellVal() + omega * (-a2_x_b[i]*p[i-1][j].cellVal() - a2_x_f[i]*p[i+1][j].cellVal()
+                //           -a2_y_b[j]*pn[i,j-1] - a2_y_f[j]*pn[i,j+1]  ) / (a2_x_c[i] + a2_y_c[j]) );
+            };
+        };
+        setBCp(p_ptr, Nx, Ny);
+        l1norm =  l1_norm (p_ptr, N_x, N_y);
+
+        ratio = ( l1norm -  l1norm_old ) / ( l1norm_old + 1e-10);
+    };
+return;
+};
+
+
--- mesh.cpp
+++ mesh.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class cell
+
+
+class FVcell
 {       
  private:
   double xC;
@@ -26,7 +28,7 @@ class cell
 
 };
 
-void cell :: setCellPos(double x, double y, double dx, double dy)
+void FVcell :: setCellPos(double x, double y, double dx, double dy)
 {
 	// x, z coordinates of the cell center of a given cell corresponding to index i, j
         xC = x; 
@@ -39,50 +41,56 @@ void cell :: setCellPos(double x, double y, double dx, double dy)
         	
 };      
 
-void cell :: setVal(double value_in)
+void FVcell :: setVal(double value_in)
 {
         //  value of the volumeField for given cell corresponding to index i, j
 	value = value_in;
 };
 
 
-void cell :: print_cellCentre(){
+void FVcell :: print_cellCentre(){
         cout << "(" <<xC<<","<<yC<<") ";};
 
-void cell :: print_cellVal(){
+void FVcell :: print_cellVal(){
         cout << value<<" ";};
 
-double cell :: cellVal(){
+double FVcell :: cellVal(){
        return this->value;};
        
-double cell :: dx(){
+double FVcell :: dx(){
        return this->xFaces[1]-this->xFaces[0];};
        
-double cell :: dy(){
+double FVcell :: dy(){
        return this->yFaces[1]-this->yFaces[0];};
 
-double cell :: xFace(int face ){
+double FVcell :: xFace(int face ){
        return this->xFaces[face];};
 
-double cell :: yFace(int face){
+double FVcell :: yFace(int face){
        return this->yFaces[face];};
 
-double cell :: xCentroid(){
+double FVcell :: xCentroid(){
        return this->xC;};
 
-double cell :: yCentroid(){
+double FVcell :: yCentroid(){
        return this->yC;};
-
+///////////////////////////////////////////////////////////////
+struct volumeField
+{
+ FVcell** mesh;
+ int dimx;
+ int dimy;
+};
 ////////////////////////////////////////////////////////////////
-cell** buildMesh (int N_x, int N_y, double L_x, double L_y)
+FVcell** buildMesh (int N_x, int N_y, double L_x, double L_y)
 {
  // index i spans the y-direction
  // index j spans the x-direction
 
- cell **mesh_p = new cell* [N_x+2];
+ FVcell **mesh_p = new FVcell* [N_x+2];
  for (int i = 0; i <= N_x+1; i++)
  {       
-   mesh_p[i] = new cell [N_y+2];
+   mesh_p[i] = new FVcell [N_y+2];
    for(int j = 0; j<= N_y+1; j++)
    {       
     mesh_p[i][j].setCellPos( L_y/N_y/2. +  (L_y/N_y)*(i-1), L_x/N_x/2. + (L_x/N_x)*(j-1), L_y/N_y, L_x/N_x  );
@@ -91,9 +99,29 @@ cell** buildMesh (int N_x, int N_y, double L_x, double L_y)
 
  return mesh_p;
 };
+////////////////////////////////////////////////////////////////
+volumeField buildFVfield (int N_x, int N_y, double L_x, double L_y)
+{
+ // index i spans the y-direction
+ // index j spans the x-direction
+ volumeField field;
+ FVcell **mesh_p = new FVcell* [N_x+2];
+ for (int i = 0; i <= N_x+1; i++)
+ {       
+   mesh_p[i] = new FVcell [N_y+2];
+   for(int j = 0; j<= N_y+1; j++)
+   {       
+    mesh_p[i][j].setCellPos( L_y/N_y/2. +  (L_y/N_y)*(i-1), L_x/N_x/2. + (L_x/N_x)*(j-1), L_y/N_y, L_x/N_x  );
+   }     
+ }       
+ field.mesh = mesh_p;
+ field.dimx = N_x;
+ field.dimy = N_y;
+ return field;
+};
 
 ////////////////////////////////////////////////////////////////
-void initFieldVal (cell** mesh_p, int N_x, int N_y, double val_in)
+void initFieldVal (FVcell** mesh_p, int N_x, int N_y, double val_in)
 {
 
  for (int i = 0; i <= N_x+1; i++)
@@ -106,7 +134,7 @@ void initFieldVal (cell** mesh_p, int N_x, int N_y, double val_in)
  return;
 };
 ////////////////////////////////////////////////////////////////
-void setBCu (cell** mesh_p, int N_x, int N_y)
+void setBCu (FVcell** mesh_p, int N_x, int N_y)
 {
  // set wall boundary condition for staggered Ux velocity field  
 
@@ -114,7 +142,7 @@ void setBCu (cell** mesh_p, int N_x, int N_y)
  for (int j= 0; j <= N_y+1; j++)
  {
     mesh_p[ 0   ][j].setVal(0);
-    mesh_p[N_x  ][j].setVal(0);
+  //  mesh_p[N_x  ][j].setVal(0);
     mesh_p[N_x+1][j].setVal(0);
  }
 
@@ -122,12 +150,12 @@ void setBCu (cell** mesh_p, int N_x, int N_y)
  for (int i = 0; i <= N_x+1; i++)
  {
     mesh_p[i][  0  ].setVal( -mesh_p[i][ 1 ].cellVal() )  ;
-    mesh_p[i][N_x+1].setVal( -mesh_p[i][N_x].cellVal() )  ;
+    mesh_p[i][N_x+1].setVal( 100 -mesh_p[i][N_x].cellVal() )  ;
  }
  return;
 };
 ////////////////////////////////////////////////////////////////
-void setBCv (cell** mesh_p, int N_x, int N_y)
+void setBCv (FVcell** mesh_p, int N_x, int N_y)
 {
  // set wall boundary condition for staggered Uy velocity field  
 
@@ -142,9 +170,29 @@ void setBCv (cell** mesh_p, int N_x, int N_y)
  for (int i = 0; i <= N_x+1; i++)
  {
     mesh_p[i][ N_y ].setVal(0);
-    mesh_p[i][N_y+1].setVal(0);
+    mesh_p[i][N_y+1].setVal( -mesh_p[i][ N_y ].cellVal() );
     mesh_p[i][  0  ].setVal(0);
+     }
+ return;
+};
 
+////////////////////////////////////////////////////////////////
+void setBCp (FVcell** mesh_p, int N_x, int N_y)
+{
+ // set wall boundary condition for staggered Uy velocity field  
+
+ // along y-axis        
+ for (int j = 0; j <= N_y+1; j++)
+ { 
+    mesh_p[  0  ][j].setVal(  mesh_p[ 1 ][j].cellVal()  );
+    mesh_p[N_x+1][j].setVal(  mesh_p[N_x][j].cellVal()  );
  }
+
+ // along x-axis        
+ for (int i = 0; i <= N_x+1; i++)
+ {
+    mesh_p[i][  0  ].setVal( mesh_p[i][ 1   ].cellVal() );
+    mesh_p[i][N_y+1].setVal( mesh_p[i][ N_y ].cellVal() );
+     }
  return;
 };
